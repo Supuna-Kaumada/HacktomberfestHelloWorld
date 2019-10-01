757767cfda5baf657cca7c5afaeefcb94ce4882f
Sahan <sahan@thinkcube.com>
2019-10-01 14:26:41 +0530

    hello world java

--- Java/HelloWorld.java
+++ Java/HelloWorld.java
@@ -5,7 +5,8 @@ public class HelloWorld {
         System.out.println("Hello, World");
 	System.out.println("Hello, Thilini");
 	System.out.println("Hello, Sewwandi");
-	System.out.println("Hello, HacktomberFestTShirt");
+    System.out.println("Hello, HacktomberFestTShirt");
+    System.out.println("Hello world : By Sahan Siriwardhan");
     }
 
 }
